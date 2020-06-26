e1e2c7a9d3e6f72100454216a2f3c947aa32e611
maxsvetlik <maxsvetlik@gmail.com>
2020-06-26 11:46:47 -0500

    minimalist compilation example in c++

---
+++ spot_comm/src/examples/auth.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpc++/grpc++.h>
+
+#include "bosdyn/api/auth_service.pb.h"
+#include "bosdyn/api/auth.pb.h"
+#include "bosdyn/api/header.pb.h"
+
+
+int main(int argc, char** argv) {
+  grpc_init();
+  return 0;
+}
+
