2dee8f24f62939788d08015220741501aa8dc2d7
maxsvetlik <maxsvetlik@gmail.com>
2020-07-03 15:35:01 -0500

    basis for jackal integration

--- spot_comm/nodes/robot_sensor_node.py
+++ spot_comm/nodes/robot_sensor_node.py
@@ -6,23 +6,25 @@ import sensor_msgs
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 
+from spot_comm.client import JackalClient
 from spot_comm.client import SpotClient
 from spot_comm.client import RobotClient
 
 class RobotSensorNode:
 
-    def __init__(self, client, poll_rate=10):
+    def __init__(self, client, poll_rate=10, robot_prefix="smads_platform"):
         self.client = client
         self.odom_pub = None
         self.scan_pub = None
         self.poll_rate = poll_rate
+        self.robot_prefix = robot_prefix
 
     def initialize_node(self):
         rospy.init_node('robot_sensor_node', anonymous=False)
         rospy.loginfo("robot_sensor_node started")
 
-        self.odom_pub = rospy.Publisher('/odom', Odometry, queue_size=10)
-        self.scan_pub = rospy.Publisher('/scan', LaserScan, queue_size=10)
+        self.odom_pub = rospy.Publisher(self.robot_prefix + '/odom', Odometry, queue_size=10)
+        self.scan_pub = rospy.Publisher(self.robot_prefix + '/scan', LaserScan, queue_size=10)
     
     def poll(self):
         r = rospy.Rate(self.poll_rate)
@@ -36,8 +38,8 @@ class RobotSensorNode:
 
 if __name__ == '__main__':
     try:
-        spot_client = SpotClient()
-        robot_sensor_node = RobotSensorNode(spot_client)
+        client = JackalClient()
+        robot_sensor_node = RobotSensorNode(client)
         robot_sensor_node.initialize_node()
         robot_sensor_node.poll()
     except rospy.ROSInterruptException:
--- spot_comm/src/spot_comm/client/__init__.py
+++ spot_comm/src/spot_comm/client/__init__.py
@@ -1,3 +1,4 @@
 from .robot_client import RobotClient
+from .jackal_client import JackalClient
 from .spot_client import SpotClient
 
