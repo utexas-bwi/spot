62e7b0709a16cd57df9696b69ef10f22c22168e7
Marika <marikamurphy@gmail.com>
2020-07-30 17:50:34 -0500

    added preliminary image service

--- spot_comm/include/spot_comm/ImageServiceImpl.h
+++ spot_comm/include/spot_comm/ImageServiceImpl.h
@@ -21,6 +21,11 @@ using bosdyn::api::GetImageRequest;
 using bosdyn::api::GetImageResponse;
 using bosdyn::api::ImageService;
 using bosdyn::api::ImageRequest;
+using bosdyn::api::ImageResponse;
+using bosdyn::api::ImageCapture;
+using bosdyn::api::ImageSource;
+
+
 namespace bosdyn {
 namespace api {
 class ImageServiceImpl final : public ImageService::Service {
