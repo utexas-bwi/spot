1a770b989b1acf822f7b029b3a3954ccac68c3fb
Shikhar Gupta <shikhar.gupta.tx@gmail.com>
2020-08-04 20:12:25 -0500

    Fixed build errors for clients

--- spot_comm/src/DirectoryServiceImpl.cpp
+++ spot_comm/src/DirectoryServiceImpl.cpp
@@ -4,16 +4,16 @@
 Status DirectoryServiceImpl::GetServiceEntry(ServerContext* context, const GetServiceEntryRequest* request,
                 GetServiceEntryResponse* response) {
   std::cout << "Service Name: " << request->service_name() << std::endl;
-  ServiceEntry* entry;
+  ServiceEntry entry;
   Timestamp timestamp = TimeUtil::GetCurrentTime();
-  entry->set_name("testService");
-  entry->set_type("testType");
-  entry->set_authority("testAuthority"); 
-  entry->set_allocated_last_update(&timestamp);
-  entry->set_user_token_required(false);
-  entry->set_permission_required("");
+  entry.set_name("testService");
+  entry.set_type("testType");
+  entry.set_authority("testAuthority"); 
+  entry.set_allocated_last_update(&timestamp);
+  entry.set_user_token_required(false);
+  entry.set_permission_required("");
   response->set_status(GetServiceEntryResponse::STATUS_OK);
-  response->set_allocated_service_entry(entry);
+  response->set_allocated_service_entry(&entry);
   return Status::OK;
 }
 
