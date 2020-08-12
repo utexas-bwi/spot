790cf85eb0c4c023e9cf7770733c44570cfd154c
geethikah21 <geethikah21@gmail.com>
2020-08-12 18:32:48 -0500

    Time sync service updates, should be complete

--- spot_comm/src/TimeSyncServiceImpl.cpp
+++ spot_comm/src/TimeSyncServiceImpl.cpp
@@ -1,37 +1,21 @@
 #include <spot_comm/TimeSyncServiceImpl.h>
 #include <spot_comm/Header.h>
 
-void fillState(TimeSyncState* state, TimeSyncEstimate* best) {
-    state->mutable_best_estimate()->CopyFrom(*best);
-    state->set_status(TimeSyncState::STATUS_OK); // may need to change this
-    state->mutable_measurement_time()->CopyFrom(TimeUtil::GetCurrentTime());
-}
-
 void fillPrevEst(TimeSyncEstimate* prev, const TimeSyncRoundTrip prev_trip) {
     Duration total_time = (prev_trip.client_rx() - prev_trip.client_tx());
     Duration server_processing = (prev_trip.server_tx() - prev_trip.server_rx());
     Duration trip_time = total_time - server_processing;
 
-    std::cout << "s: " << trip_time.seconds() << " ns: " << trip_time.nanos() << std::endl;
-
     Duration skew;
     skew = (prev_trip.server_rx() - prev_trip.client_tx()) + (prev_trip.server_tx() - prev_trip.client_rx());
     skew /= 2;
-
-    std::cout << "skew: " << skew.seconds() << std::endl;
 
     prev->mutable_round_trip_time()->CopyFrom(trip_time);
     prev->mutable_clock_skew()->CopyFrom(skew);
 }
 
-void fillBestEst(TimeSyncEstimate* best, TimeSyncEstimate* prev) {
-    best->mutable_round_trip_time()->CopyFrom(prev->round_trip_time());
-    best->mutable_clock_skew()->CopyFrom(prev->clock_skew());
-}
-
 void fillHeader(ResponseHeader* header, const RequestHeader reqHeader) {
     header->mutable_request_received_timestamp()->CopyFrom(TimeUtil::GetCurrentTime());
-    // header->mutable_request_received_timestamp()->set_seconds(header->request_received_timestamp().seconds());
     header->mutable_request_header()->CopyFrom(reqHeader);
 }
 
@@ -39,29 +23,13 @@ Status TimeSyncServiceImpl::TimeSyncUpdate(ServerContext* context, const TimeSyn
     // Mark when request was received and copy over request header    
     fillHeader(response->mutable_header(), request->header());
     
-    // std::cout << "Repsonse start" << std::endl;
-
     // Calculate round trip time and clock skew from previous exchange
     fillPrevEst(response->mutable_previous_estimate(), request->previous_round_trip());
 
-    // std::cout << "Repsonse start 5" << std::endl;
-
-    // Update best estimate if applicable
-    fillBestEst(response->mutable_state()->mutable_best_estimate(), response->mutable_previous_estimate());
-
-    // std::cout << "Repsonse start 7" << std::endl;
-
-    // Update state fields
-    fillState(response->mutable_state(), response->mutable_state()->mutable_best_estimate());
-
-    // std::cout << "Repsonse start 8" << std::endl;
-
-    // Populate rest of response fields -- clock idenfier and document when response was sent
+    // Populate rest of response fields -- clock identifier and document when response was sent
     response->set_clock_identifier(request->clock_identifier());
+    response->mutable_state()->set_status(TimeSyncState::STATUS_OK);
     response->mutable_header()->mutable_response_timestamp()->CopyFrom(TimeUtil::GetCurrentTime());
-    // response->mutable_header()->mutable_response_timestamp()->set_seconds(response->header().response_timestamp().seconds());
-
-    // std::cout << "Response end" << std::endl;
 
     return Status::OK;
 }
--- spot_comm/src/time_sync_client.cpp
+++ spot_comm/src/time_sync_client.cpp
@@ -38,29 +38,17 @@ class TimeSyncClient {
     // the server and/or tweak certain RPC behaviors.
     ClientContext context;
 
-    std::cout << "Client start got here" << std::endl;
-
     request.mutable_header()->mutable_request_timestamp()->CopyFrom(TimeUtil::GetCurrentTime());
 
     // The actual RPC.
     Status status = stub_->TimeSyncUpdate(&context, request, &reply);
 
-    // std::cout << reply.state().status() << std::endl;
-    // std::cout << reply.header().request_received_timestamp() << std::endl;
-    // std::cout << reply.header().response_timestamp() << std::endl;
-    // std::cout << reply.previous_estimate().round_trip_time() << std::endl;
-    // std::cout << reply.previous_estimate().clock_skew() << std::endl;
-
-    std::cout << "Client end got here" << std::endl;
-
-
     // Act upon its status.
-    if (status.ok()) {
-      std::cout << "Status: " << reply.state().status() << std::endl;
-    } else {
+    if(!status.ok()) {
       std::cout << status.error_code() << ": " << status.error_message()
                 << std::endl;
     }
+
     return reply;
   }
 
@@ -68,45 +56,47 @@ class TimeSyncClient {
     const std::string clock_identifier("spot_time_sync");
     TimeSyncUpdateResponse reply;
     TimeSyncUpdateRequest request;
-    TimeSyncEstimate best;
-    best.mutable_round_trip_time()->set_seconds(10000000); // is this needed?
+    Duration averageSkew;
+    // Keep track of best estimate overall
+    TimeSyncState best;
+    best.mutable_best_estimate()->mutable_round_trip_time()->set_seconds(INT64_MAX);
+    int numLows = 0;
 
-    for(int i = 0; i <= numRounds && (reply.state().status() == 1 || i == 0); i++) {
+    for(int i = 0; i <= numRounds && (reply.state().status() != TimeSyncState::STATUS_UNKNOWN || i == 0); i++) {
       request.set_clock_identifier(clock_identifier);
+
       reply = TimeSyncUpdate(request, clock_identifier);
 
+      // Update timestamps from previous round trip
       request.mutable_previous_round_trip()->mutable_client_rx()->CopyFrom(TimeUtil::GetCurrentTime());
       request.mutable_previous_round_trip()->mutable_client_tx()->CopyFrom(reply.header().request_header().request_timestamp());
       request.mutable_previous_round_trip()->mutable_server_tx()->CopyFrom(reply.header().response_timestamp());
       request.mutable_previous_round_trip()->mutable_server_rx()->CopyFrom(reply.header().request_received_timestamp());
-
-      std::cout << i << std::endl;
-
-      std::cout << reply.clock_identifier() << std::endl;
       
+      // Finding best time sync estimate: determine the lowest round trip delay
+      // and averaging the clock skew numbers of all the samples with this minimum round trip time to reduce noise
       if(i > 0) {
-        if(reply.previous_estimate().round_trip_time() == best.round_trip_time()) {
-          std::cout << "averaging" << std::endl;
-          Duration averageSkew = best.clock_skew() + reply.previous_estimate().clock_skew();
-          averageSkew /= 2; 
-          std::cout << "new skew: " << averageSkew << std::endl;
-          best.mutable_clock_skew()->CopyFrom(averageSkew);
+        if(reply.previous_estimate().round_trip_time() == best.best_estimate().round_trip_time()) {
+          averageSkew = (best.best_estimate().clock_skew() * numLows) + reply.previous_estimate().clock_skew();
+          numLows++;
+          averageSkew /= numLows; 
+          best.mutable_best_estimate()->mutable_clock_skew()->CopyFrom(averageSkew);
+          best.mutable_measurement_time()->CopyFrom(TimeUtil::GetCurrentTime());
         }
-        else if(reply.previous_estimate().round_trip_time() < best.round_trip_time()) {
-          std::cout << "copying" << std::endl;
-          best.mutable_round_trip_time()->CopyFrom(reply.previous_estimate().round_trip_time());
-          best.mutable_clock_skew()->CopyFrom(reply.previous_estimate().clock_skew());
-        }
-        else {
-          std::cout << "not copying" << std::endl;
+        else if(reply.previous_estimate().round_trip_time() < best.best_estimate().round_trip_time()) {
+          numLows = 1;
+          best.mutable_best_estimate()->mutable_round_trip_time()->CopyFrom(reply.previous_estimate().round_trip_time());
+          best.mutable_best_estimate()->mutable_clock_skew()->CopyFrom(reply.previous_estimate().clock_skew());
+          best.mutable_measurement_time()->CopyFrom(TimeUtil::GetCurrentTime());
         }
       }
+
+      reply.mutable_state()->set_status(TimeSyncState::STATUS_SERVICE_NOT_READY);
     }
 
-    reply.mutable_state()->mutable_best_estimate()->CopyFrom(best);
+    reply.mutable_state()->CopyFrom(best);
+    reply.mutable_state()->set_status(TimeSyncState::STATUS_OK);
 
-    std::cout << "best estimate rt: " << reply.state().best_estimate().round_trip_time() << std::endl;
-    std::cout << "best estimate cs: " << reply.state().best_estimate().clock_skew() << std::endl;
     return reply;
   }
 
@@ -148,6 +138,12 @@ int main(int argc, char** argv) {
 
   if(reply.state().status() == 1) {
     std::cout << "Time sync completed: " << reply.state().status() << std::endl;
+    std::cout << "ID: " << reply.clock_identifier() << std::endl;
+    std::cout << "best estimate round trip delay: " << reply.state().best_estimate().round_trip_time() << std::endl;
+    std::cout << "best estimate clock skew: " << reply.state().best_estimate().clock_skew() << std::endl;
+  }
+  else {
+    std::cout << "Time sync service error" << std::endl;
   }
 
   return 0;
