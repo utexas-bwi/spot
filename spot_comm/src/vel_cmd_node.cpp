1e4471bd61375404691fd670b5a40b53a0318a0e
geethikah21 <geethikah21@gmail.com>
2020-08-30 10:46:22 -0500

    Added a node that just publishes a twist message given lin/ang velocities

---
+++ spot_comm/src/vel_cmd_node.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include "spot_comm/VelocityCommand.h"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "vel_cmd_node");
+  ros::NodeHandle n;
+
+  VelocityCommand vel(n);
+  
+  double xVel;
+  double yVel;
+  double angularVel;
+
+  int counter = 0;
+
+  ros::Rate r(1000);
+
+  while(ros::ok() && counter < 2000) {
+    xVel = 1;
+    yVel = 1;
+    angularVel = 0;
+
+    vel.executeCommand(xVel, yVel, angularVel);
+
+    counter++;
+    
+    if(counter == 2000) {
+      vel.executeCommand(0, 0, 0);
+    }
+    
+    r.sleep();
+    ros::spinOnce();
+  }
+
+  return 0;
+}
